d2b9b0172e7ce2cf8b14a1da589591bd29785b3d
seongmin <psm7335@naver.com>
2021-02-15 17:15:01 +0900

    1010번: 다리 놓기
    수학
    다이나믹 프로그래밍
    조합론
    
    combination성질 이용한 다이나믹 프로그래밍

---
+++ Mathematics/Bridge1010.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bridge1010 {
+    static int[][] bridge = new int[30][30];
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for(int i=0; i<t; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            System.out.println(combination(m,n));
+        }
+
+
+
+
+    }
+    static int combination(int n, int r){ //조합 
+        if(bridge[n][r]>0){
+            return bridge[n][r];
+        }
+
+        if(n==r || r==0){
+            return bridge[n][r]=1;
+        }
+
+        return combination(n-1, r-1) + combination(n-1, r);
+    }
+}
