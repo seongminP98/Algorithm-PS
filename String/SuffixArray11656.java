1e0f6802f4ce78ca7978fd758131e37888f4d866
seongmin <psm7335@naver.com>
2021-03-22 22:17:41 +0900

    11656번: 접미사 배열
    문자열
    정렬

---
+++ String/SuffixArray11656.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class SuffixArray11656 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String[] arr = new String[s.length()];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = s.substring(i);
+        }
+        Arrays.sort(arr);
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
