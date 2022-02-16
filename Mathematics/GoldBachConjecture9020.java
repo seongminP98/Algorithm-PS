9e35ad234e1e6c1a6ccf9a0af5d0585de8a1981a
seongmin <psm7335@naver.com>
2022-02-16 17:54:30 +0900

    9020번: 골드바흐의 추측
    수학
    정수론
    소수 판정
    에라토스테네스의 체
    *시간 최적화

--- Mathematics/GoldBachConjecture9020.java
+++ Mathematics/GoldBachConjecture9020.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+
 
 public class GoldBachConjecture9020 {
     public static void main(String[] args) throws Exception {
@@ -15,34 +13,21 @@ public class GoldBachConjecture9020 {
                 }
             }
         }
-        List<Integer> primes = new ArrayList<>();
-        for (int i = 2; i <= 10000; i++) {
-            if (!isPrime[i])
-                primes.add(i);
-        }
+
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int t = 0; t < T; t++) {
             int n = Integer.parseInt(br.readLine());
-            int value1 = 0;
-            int value2 = 0;
-            int min = Integer.MAX_VALUE;
-            for (int i = 0; i < primes.size(); i++) {
-                if (primes.get(i) > n)
+            int p = n/2;
+            int q = n/2;
+            while(true) {
+                if(!isPrime[p] && !isPrime[q]) {
+                    sb.append(p).append(" ").append(q).append('\n');
                     break;
-                for (int j = i; j < primes.size(); j++) {
-                    if (primes.get(j) > n)
-                        break;
-
-                    if (primes.get(i) + primes.get(j) == n) {
-                        if (min > primes.get(j) - primes.get(i)) {
-                            value1 = primes.get(i);
-                            value2 = primes.get(j);
-                        }
-                    }
                 }
+                p--;
+                q++;
             }
-            sb.append(value1).append(" ").append(value2).append('\n');
         }
         System.out.println(sb);
     }
