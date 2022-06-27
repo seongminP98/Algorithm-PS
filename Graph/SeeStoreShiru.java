f232dd1d9661762d6d491cc9453103af060a779e
seongmin <psm7335@naver.com>
2022-06-28 01:11:36 +0900

    25307번: 시루의 백화점 구경
    그래프 이론
    그래프 탐색
    너비 우선 탐색

--- Graph/SeeStoreShiru.java
+++ Graph/SeeStoreShiru.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class SeeStoreShiru {
     static int N, M, K;
     static int[][] arr;
-    static List<Node> mannequins;
     static Node start;
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0};
@@ -17,7 +16,6 @@ public class SeeStoreShiru {
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         arr = new int[N][M];
-        mannequins = new ArrayList<>();
         /*
         빈 칸 0
         기둥 1 - 갈 수 없음
@@ -25,22 +23,21 @@ public class SeeStoreShiru {
         마네킹 3 - K만큼 떨어져야 함
         시작 위치 4
          */
+        Queue<Node> maneQ = new LinkedList<>();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
                 if (arr[i][j] == 3) {
 
-
-                    mannequins.add(new Node(i, j));
+                    maneQ.add(new Node(i, j, 0));
                 } else if (arr[i][j] == 4) {
                     start = new Node(i, j, 0);
                 }
             }
         }
 
-        setNearMane();
-
+        bfsNearMane(maneQ);
         bfs();
         System.out.println(-1);
     }
@@ -66,21 +63,20 @@ public class SeeStoreShiru {
         }
     }
 
-    private static void setNearMane() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (arr[i][j] == 0 || arr[i][j] == 2) {
-                    for (Node mannequin : mannequins) {
-                        if (Math.abs(i - mannequin.x) + Math.abs(j - mannequin.y) <= K) {
-
-                            arr[i][j] = 1;
-                            break;
-                        }
-                    }
+    private static void bfsNearMane(Queue<Node> q) {
+        while (!q.isEmpty()) {
+            Node c = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = c.x + dx[i];
+                int ny = c.y + dy[i];
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M && (arr[nx][ny] == 0 || arr[nx][ny] == 2) && c.health < K) {
+                    arr[nx][ny] = 1;
+                    q.add(new Node(nx, ny, c.health + 1)); // 여기서 health 는 거리
                 }
             }
         }
     }
+
 
     private static class Node {
         int x, y, health;
