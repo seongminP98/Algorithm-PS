5b2b49289d33e7271c96e0832f4eb2cacd14d1dd
seongmin <psm7335@naver.com>
2022-07-26 22:26:13 +0900

    18235번: 지금 만나러 갑니다
    그래프 이론
    그래프 탐색
    너비 우선 탐색

--- Graph/GoingMeetNow18235.java
+++ Graph/GoingMeetNow18235.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class GoingMeetNow18235 {
     static int N, A, B;
-    static int[] da = {1, -1, 1, -1};
-    static int[] db = {-1, 1, 1, -1};
+    static int[] d = {1, -1};
+
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,61 +17,57 @@ public class GoingMeetNow18235 {
             System.out.println(-1);
             return;
         }
-        Queue<Duck> q = new LinkedList<>();
 
         boolean[][] visitedA = new boolean[N + 1][20];
         boolean[][] visitedB = new boolean[N + 1][20];
-//        visitedA[A][0] = true;
-//        visitedB[B][0] = true;
-        q.add(new Duck(A, B, 0));
-        while (!q.isEmpty()) {
-            Duck c = q.poll();
-            System.out.println("c = " + c);
-            if (c.a == c.b) {
-                System.out.println(c.cnt);
-                return;
-            }
-            if (c.cnt >= 20) {
-                System.out.println(-1);
-                return;
-            }
-            for (int i = 0; i < 4; i++) {
-                int na = c.a + (int) Math.pow(2, c.cnt) * da[i];
-                int nb = c.b + (int) Math.pow(2, c.cnt) * db[i];
-                if (na > 0 && na <= N && nb > 0 && nb <= N) {
-                    System.out.println("visitedA[na][c.cnt] = " + visitedA[na][c.cnt]);
-                    System.out.println("visitedB[nb][c.cnt] = " + visitedB[nb][c.cnt]);
-                    System.out.println("na = " + na + " nb = " + nb + " c.cnt = " + c.cnt);
 
-                    if (!visitedA[na][c.cnt] || !visitedB[nb][c.cnt]) {
-                        visitedA[na][c.cnt] = true;
-                        visitedB[nb][c.cnt] = true;
-                        q.add(new Duck(na, nb, c.cnt + 1));
+        Queue<Integer> qA = new LinkedList<>();
+        Queue<Integer> qB = new LinkedList<>();
+        qA.add(A);
+        qB.add(B);
+        int cnt = 0;
+        while (!qA.isEmpty()) {
+            int size = qA.size();
+            while (size-- > 0) {
+                int c = qA.poll();
+                if (cnt >= 20) {
+                    break;
+                }
+                for (int i = 0; i < 2; i++) {
+                    int na = c + (int) Math.pow(2, cnt) * d[i];
+                    if (na > 0 && na <= N && !visitedA[na][cnt + 1]) {
+                        qA.add(na);
+                        visitedA[na][cnt + 1] = true;
                     }
                 }
             }
+
+            cnt++;
+        }
+
+        cnt = 0;
+        while (!qB.isEmpty()) {
+            int size = qB.size();
+            while (size-- > 0) {
+                int c = qB.poll();
+                if (visitedA[c][cnt]) {
+                    System.out.println(cnt);
+                    System.exit(0);
+                }
+                if (cnt >= 20) {
+                    break;
+                }
+                for (int i = 0; i < 2; i++) {
+                    int nb = c + (int) Math.pow(2, cnt) * d[i];
+                    if (nb > 0 && nb <= N && !visitedB[nb][cnt + 1]) {
+                        qB.add(nb);
+                        visitedB[nb][cnt + 1] = true;
+                    }
+                }
+            }
+            cnt++;
         }
         System.out.println(-1);
     }
 
-
-    private static class Duck {
-        int a, b, cnt;
-
-        public Duck(int a, int b, int cnt) {
-            this.a = a;
-            this.b = b;
-            this.cnt = cnt;
-        }
-
-
-        @Override
-        public String toString() {
-            return "Duck{" +
-                    "a=" + a +
-                    ", b=" + b +
-                    ", cnt=" + cnt +
-                    '}';
-        }
-    }
 }
